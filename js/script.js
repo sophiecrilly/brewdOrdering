40b8b813511ee29ce9d3b7784828c9b73985012b
Sophie Crilly <sophiecrilly@Sophies-MacBook-Pro.local>
2020-04-18 22:44:10 +1200

    home page done and navigation almost

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$('#navListButton').click(function(){
+    $('#navList').slideToggle(300);
+});
+
+$('#navListButton').click(function(){
+    $('#navButtons').toggle();
+});
+
+$('#closeButton').click(function(){
+    $('#navButtons').toggle();
+});
+
+$('#closeButton').click(function(){
+    $('#navList').slideToggle(300);
+});
